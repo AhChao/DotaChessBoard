758fffacbfdcb11a04c09272ae38ef6f33d20752
YuLinChao <gash612@gmail.com>
2019-01-24 21:43:39 +0800

    基本功能完成
    大致上基本功能完成
    除了跑bar動畫會從頭開始跑以外
    然後英文版還有部分字串沒翻譯,圖沒截

--- src/language.js
+++ src/language.js
@@ -2,11 +2,11 @@ function languageSetting(lan)
 {
 	//lan 0 = ch 1 = en
 	var settingList = [
-	"pageTitle","fieldText","addBtn","fieldAblity"
+	"pageTitle","fieldText","addBtn","fieldAblity","raceText","jobText"
 	]
 	var pageText = [
-	["刀塔自走棋記錄板","場上","添加棋子","場上能力"],
-	["Dota Auto Chess Board","field","add","ablity"]
+	["刀塔自走棋記錄板","場上","添加棋子","場上能力","種族","職業"],
+	["Dota Auto Chess Board","Field","Add","Ablity","Species","Classes"]
 	]
 	for(var i in settingList)
 	{
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+var fieldChess = [];
 function switchAblity(tar)
 {
 	if(tar==1)
@@ -20,6 +21,7 @@ function switchAblity(tar)
 //填充目標 目前長度 總長度
 function barMove(barName,now,target)
 {
+	console.log(barName,now,target);
     var elem = document.getElementById(barName+"Bar"); 
     var width = 1;
     var id = setInterval(frame, 10);
@@ -34,6 +36,30 @@ function barMove(barName,now,target)
     }
 }
 
+function refreshFieldTable()
+{
+	document.getElementById("fieldTableDiv").innerHTML = "";
+	d3.select("#fieldTableDiv").append("table").attr("id","fieldTable");
+	
+	var j = 0;
+	for(var i in fieldChess)
+	{
+		if(j == 0)
+		{
+			d3.select("#nowFTR").attr("id",null);
+			d3.select("#fieldTable").append("tr").attr("id","nowFTR");
+			j=5;
+		}
+		d3.select("#nowFTR").append("td").append("img").
+		attr({
+			"src":"./img/ch/"+fieldChess[i]+".png",
+			"chessNo":fieldChess[i],
+			"onclick":"removeChess($(this).attr('chessNo'))",
+		});
+		j--;
+	}
+}
+
 function refreshAddTable()
 {
 	var tableContent = [];
@@ -43,22 +69,119 @@ function refreshAddTable()
 		tableContent.push(item);
 		item++;
 	}
-	console.log(tableContent);
+	//var opt1 = d3.select("#optRarity")[0][0].value;
+	var opt2 = d3.select("#optRace")[0][0].value;
+	var opt3 = d3.select("#optJob")[0][0].value;
+	var delList = [];
+	/*for(var i in originData)
+	{
+		for(var j in originData[i][0])
+		{
+			if(originData[i][0][j]==opt2)
+			{
+				tableContent.push(i);
+				break;
+			}
+		}		
+	}*/
+	if(opt2!=0)
+	for(var i in tableContent)
+	{
+		var isin = 0;
+		for(var j in originData[tableContent[i]][0])
+		{
+			if(originData[tableContent[i]][0][j]==opt2) isin=1;
+		}
+		if(isin==0) delList.push(tableContent[i]);
+	}
+	tableContent = tableContent.filter( function( el ) {
+	  return delList.indexOf( el ) < 0;
+	} );
+	delList=[];
+	if(opt3!=0)
+	for(var i in tableContent)
+	{
+		var isin = 0;
+		for(var j in originData[tableContent[i]][1])
+		{
+			if(originData[tableContent[i]][1][j]==opt3) isin=1;
+		}
+		if(isin==0) delList.push(tableContent[i]);
+	}
+	tableContent = tableContent.filter( function( el ) {
+	  return delList.indexOf( el ) < 0;
+	} );
+
 	document.getElementById("addTableDiv").innerHTML = "";
 	d3.select("#addTableDiv").append("table").attr("id","addTable");
 	
 	var j = 0;
 	for(var i in tableContent)
 	{
-		console.log(i);
 		if(j == 0)
 		{
-			d3.select("#nowTR").attr("id",null);
-			d3.select("#addTable").append("tr").attr("id","nowTR");
+			d3.select("#nowATR").attr("id",null);
+			d3.select("#addTable").append("tr").attr("id","nowATR");
 			j=5;
 		}
-		d3.select("#nowTR").append("td").append("img").attr("src","./img/ch/"+tableContent[i]+".png");
+		d3.select("#nowATR").append("td").append("img").
+		attr({
+			"src":"./img/ch/"+tableContent[i]+".png",
+			"chessNo":tableContent[i],
+			"onclick":"addChess($(this).attr('chessNo'))",
+		});
 		j--;
+	}
+}
+
+function addChess(chessNo)
+{
+	for(var i in fieldChess)
+	{
+		if(fieldChess[i]==chessNo)
+			return 0;
+	}
+	fieldChess.push(chessNo);
+	refreshFieldTable();
+	updateProgress();
+}
+
+function removeChess(chessNo)
+{
+	for(var i in fieldChess)
+	{
+		if(fieldChess[i]==chessNo)
+			fieldChess.splice(i,1);
+	}
+	refreshFieldTable();
+	updateProgress();
+}
+
+function updateProgress()
+{	
+	var raceValue = [0,0,0,0,0,0,0,0,0,0,0];
+	var jobValue = [0,0,0,0,0,0,0,0,0,0];
+	var raceLimit = [0,6,6,4,4,3,6,4,6,4,2];
+	var jobLimit = [0,6,6,6,6,6,4,6,2,4];
+	var raceName = ["","gnome","beast","undead","naga","dragon","human","orc","elf","troll","element"];
+	var jobName = ["","warrior","mage","assassin","hunter","knight","druid","warlock","shaman","mech"];
+	for(var i in fieldChess)
+	{
+		for(var j in originData[fieldChess[i]][0])
+			raceValue[originData[fieldChess[i]][0][j]]++;
+		for(var j in originData[fieldChess[i]][1])
+			jobValue[originData[fieldChess[i]][1][j]]++;
+	}
+	console.log("update",fieldChess,raceValue,jobValue);
+	for(var i in raceValue)
+	{
+		if(i==0) continue;
+		barMove(raceName[i],raceValue[i],raceLimit[i]);
+	}
+	for(var i in jobValue)
+	{
+		if(i==0) continue;
+		barMove(jobName[i],jobValue[i],jobLimit[i]);
 	}
 }
 
@@ -71,4 +194,12 @@ function addBtnClick()
 		d3.select("#addDiv").attr("style",null);
 		refreshAddTable();
 	}
+}
+
+function clearFilter()
+{
+	//document.getElementById("optRarity").selectedIndex = 0;
+	document.getElementById("optRace").selectedIndex = 0;
+	document.getElementById("optJob").selectedIndex = 0;
+	refreshAddTable();
 }

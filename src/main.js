55e0d07ab941b3063ae83b76864a9f820e14faf2
YuLinChao <gash612@gmail.com>
2019-01-24 18:53:53 +0800

    版型調整
    拉好更多版型，使用手機端測試瀏覽

--- src/main.js
+++ src/main.js
@@ -15,3 +15,60 @@ function switchAblity(tar)
 		d3.select("#raceCountText").attr("style",null);
 	}
 }
+
+//barMove('gnome',1,6)
+//填充目標 目前長度 總長度
+function barMove(barName,now,target)
+{
+    var elem = document.getElementById(barName+"Bar"); 
+    var width = 1;
+    var id = setInterval(frame, 10);
+    document.getElementById(barName+"Label").innerHTML = now +"/" + target;
+    function frame() {
+        if (width >= ((now/target)*100)) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
+
+function refreshAddTable()
+{
+	var tableContent = [];
+	var item = 1;
+	while(item<53)
+	{
+		tableContent.push(item);
+		item++;
+	}
+	console.log(tableContent);
+	document.getElementById("addTableDiv").innerHTML = "";
+	d3.select("#addTableDiv").append("table").attr("id","addTable");
+	
+	var j = 0;
+	for(var i in tableContent)
+	{
+		console.log(i);
+		if(j == 0)
+		{
+			d3.select("#nowTR").attr("id",null);
+			d3.select("#addTable").append("tr").attr("id","nowTR");
+			j=5;
+		}
+		d3.select("#nowTR").append("td").append("img").attr("src","./img/ch/"+tableContent[i]+".png");
+		j--;
+	}
+}
+
+function addBtnClick()
+{
+	if(d3.select("#addDiv").attr("style")==null)
+		d3.select("#addDiv").attr("style","display:none;");
+	else
+	{
+		d3.select("#addDiv").attr("style",null);
+		refreshAddTable();
+	}
+}
